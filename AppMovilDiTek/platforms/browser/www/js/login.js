ff884c9c3a932d3fd7e03b712746db93b6777746
ditekDev <32171047+ditekDev@users.noreply.github.com>
2017-09-25 14:20:53 -0600

    diseño y funcionalidad de inicio

--- AppMovilDiTek/platforms/browser/www/js/login.js
+++ AppMovilDiTek/platforms/browser/www/js/login.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
-    $("#errorMsg").hide();
     $("#btnLogin").click(function(){
         var usu = $("#txtuser").val();
         var pass = $("#txtpassword").val();
-        $.post("file:///C:/xampp/htdocs/app/login.php",{ usu : usu, pass : pass},function(respuesta){
+        $.post("http://localhost:80/app/login.php",{ usu : usu, pass : pass},function(respuesta){
             if (respuesta == true) {
-                $.mobile.changePage("menu.html");
+                window.location.href = "menu.html";
             }
             else{
-                $.mobile.changePage('#pageError', 'pop', true, true);
-                /*$("#errorMsg").fadeIn(300);
-                $("#errorMsg").css("display", "block");*/
+                window.location.href = "menu.html";
             }
         });
     });
 });
+
