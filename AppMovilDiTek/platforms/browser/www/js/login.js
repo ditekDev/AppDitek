f044a8ac04e723f768cca8546de295ee3e7f0f7b
ditekDev <32171047+ditekDev@users.noreply.github.com>
2017-09-27 09:23:53 -0600

    inicio funcional

--- AppMovilDiTek/platforms/browser/www/js/login.js
+++ AppMovilDiTek/platforms/browser/www/js/login.js
@@ -1,13 +1,22 @@
-$(document).ready(function(){
+function valida(){
+	$(document).ready(function(){
     $("#btnLogin").click(function(){
         var usu = $("#txtuser").val();
         var pass = $("#txtpassword").val();
-        $.post("http://localhost:80/app/login.php",{ usu : usu, pass : pass},function(respuesta){
+        if(!usu===""||pass===""){
+        	$.post("http://localhost:80/app/login.php",{ usu : usu, pass : pass},function(respuesta){
             if (respuesta == true) {
                 window.location.href = "menu.html";
             }
-            
-        });
+            else{
+               alert("error");
+            }
+            });
+
+        }
+        
     });
 });
+}
+
 
--- AppMovilDiTek/www/js/login.js
+++ AppMovilDiTek/www/js/login.js
@@ -1,13 +1,25 @@
-$(document).ready(function(){
+function valida(){
+	$(document).ready(function(){
     $("#btnLogin").click(function(){
         var usu = $("#txtuser").val();
         var pass = $("#txtpassword").val();
-        $.post("http://localhost:80/app/login.php",{ usu : usu, pass : pass},function(respuesta){
+        if(!usu===""||pass===""){
+        	//validaciones
+
+        }else{
+        	//llama funcion del php
+        	$.post("http://localhost:80/app/login.php",{ usu : usu, pass : pass},function(respuesta){
             if (respuesta == true) {
                 window.location.href = "menu.html";
             }
-            
-        });
+            else{
+               alert("El usuario y la contraseña no coinciden");
+            }
+            });
+        }
+        
     });
 });
+}
+
 
