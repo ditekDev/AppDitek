dd88dfecf603f7d9e74ead1b14ca96348d5ad837
ditekDev <32171047+ditekDev@users.noreply.github.com>
2017-10-12 14:02:27 -0600

    captura

--- AppMovilDiTek/platforms/android/assets/www/js/login.js
+++ AppMovilDiTek/platforms/android/assets/www/js/login.js
@@ -1,5 +1,5 @@
 function inicio(){
-    var db = openDatabase("diteklocal", "1.0", "Sync DB", 200000);
+    var db = openDatabase("diteklocal", "1.0", "db", 200000);
     var msg;
     var usu=document.getElementsByName("txtuser")[0].value;	
     var pass=document.getElementsByName("txtpassword")[0].value;	
@@ -8,13 +8,13 @@ function inicio(){
     
 	}else{
     	db.transaction(function (tx) {
-        	tx.executeSql('SELECT * FROM employee WHERE usuario="'+usu+'" AND contrasena="'+pass+'"', [], function (tx, results) {
+        	tx.executeSql('SELECT * FROM usuarios WHERE usuario="'+usu+'" AND contrasena="'+pass+'"', [], function (tx, results) {
        		var len = results.rows.length, i;
            
        		if (len===1) {
                   window.location.href = "menu.html";
         	}else{
-                 alert("Incorrecto");
+                window.location.href = "errorinicio.html";
         	}  
         }, null);
         });
