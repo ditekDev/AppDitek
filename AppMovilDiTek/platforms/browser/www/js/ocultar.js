07e7ec18d4aec7e80a3e9f598a00eea870ea5cd8
ditekDev <32171047+ditekDev@users.noreply.github.com>
2017-10-25 14:57:56 -0600

    index

--- AppMovilDiTek/platforms/browser/www/js/ocultar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    if (localStorage.getItem("cloro")=="1") {
-        $("#cloro").css({"display":"none"});
-    }
-    if (localStorage.getItem("ph")=="1") {
-        $("#ph").css({"display":"none"});
-    }
-    
-   });
