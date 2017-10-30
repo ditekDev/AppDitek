3d113b0708a5bdc412a73bf8dcdd73ad6134a4ab
ditekDev <32171047+ditekDev@users.noreply.github.com>
2017-10-30 13:28:37 -0600

    tabla abonados

--- AppMovilDiTek/www/js/lectura.js
+++ AppMovilDiTek/www/js/lectura.js
@@ -2,7 +2,7 @@
 
 var self = this;
 var db;
-this.db= openDatabase('diteklocal', '1.0', 'Test DB', 2 * 1024 * 1024);
+this.db= openDatabase('diteklocal', '1.0', 'db', 2 * 1024 * 1024);
 
 
 function lecturaMed(){
@@ -39,7 +39,7 @@ function lecturaMed(){
     
     
     
-    function crearTabla() {
+    function crearTablaMedidores() {
     this.db.transaction(
         function(tx) {
             var sql ='CREATE TABLE IF NOT EXISTS medidores (id_abonado, numero_medidor)';
@@ -49,21 +49,10 @@ function lecturaMed(){
           
     );
     };
-    
-
-function crearTabla() {
-this.db.transaction(
-    function(tx) {
-        var sql ='CREATE TABLE IF NOT EXISTS medidores (id_abonado, numero_medidor)';
-        tx.executeSql(sql);
-    },
-    this.txErrorHandler,
-      
-);
-};
 
 
-function borrarTabla(){
+
+function borrarTablaMedidores(){
 this.db.transaction(
     function(tx) {
         tx.executeSql('DROP TABLE IF EXISTS medidores');
@@ -72,13 +61,13 @@ this.db.transaction(
 );
 };
 
-function lee_json() {
+function lee_jsonMedidores() {
 
 $.ajax({
     dataType: 'json',
     url: 'http://grupoditek.com/php/getMedidores.php',
     success: function(datos) {
-        var db = openDatabase('diteklocal', '1.0', 'Test DB', 2 * 1024 * 1024);
+        var db = openDatabase('diteklocal', '1.0', 'db', 2 * 1024 * 1024);
         db.transaction(
             function(tx) {
                 var l = datos.length;
@@ -102,15 +91,82 @@ $.ajax({
 });     
 } ;
 
-function sincro(){
+function sincroMedidores(){
     var con = localStorage.getItem("conexion");
     if (con=="1") {
-        borrarTabla();
-        crearTabla();
-        lee_json();
+        //borrarTablaMedidores();
+        crearTablaMedidores();
+        lee_jsonMedidores();
     }
    
 };
 
+
+
+
+
+
+
+function crearTablaAbonados() {
+    this.db.transaction(
+        function(tx) {
+            var sql ='CREATE TABLE IF NOT EXISTS abonados (nombre,direccion,id_abonado)';
+            tx.executeSql(sql);
+        },
+        this.txErrorHandler,
+          
+    );
+    };
+
+
+
+function borrarTablaAbonados(){
+this.db.transaction(
+    function(tx) {
+        tx.executeSql('DROP TABLE IF EXISTS abonados');
+    },
+    this.txErrorHandler,
+);
+};
+
+function lee_jsonAbonados() {
+
+$.ajax({
+    dataType: 'json',
+    url: 'http://grupoditek.com/php/getAbonados.php',
+    success: function(datos) {
+        var db = openDatabase('diteklocal', '1.0', 'db', 2 * 1024 * 1024);
+        db.transaction(
+            function(tx) {
+                var l = datos.length;
+                var sql =
+                    "INSERT OR REPLACE INTO abonados (nombre,direccion,id_abonado) VALUES (?, ?, ?)";
+    
+                var e;
+                for (var i = 0; i < l; i++) {
+                    e = datos[i];
+                   
+                    var params = [e.nombre,e.direccion,e.id_abonado];
+                    tx.executeSql(sql, params);
+                }
+             
+            },
+            this.txErrorHandler,
+        
+        );
+    },
+    error: function() { myapp.alert('No se conecto al servidor. Intente de nuevo','ERROR!!!'); }
+});     
+} ;
+
+function sincroAbonados(){
+    var con = localStorage.getItem("conexion");
+    if (con=="1") {
+        //borrarTablaMedidores();
+        crearTablaAbonados();
+        lee_jsonAbonados();
+    }
+   
+};
 
 
