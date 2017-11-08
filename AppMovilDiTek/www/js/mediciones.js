8bbc7f6769c6a31e954432c2b35af5d81fc3d6a8
ditekDev <32171047+ditekDev@users.noreply.github.com>
2017-11-08 12:30:46 -0600

    turbidez temperatura

--- AppMovilDiTek/www/js/mediciones.js
+++ AppMovilDiTek/www/js/mediciones.js
@@ -60,7 +60,61 @@ function insertarCalidadFuentePH(){
     );
 };
 
+function insertarCalidadFuenteTemperatura(){
+    var db = openDatabase('diteklocal', '1.0', 'DB', 2 * 1024 * 1024);
+    db.transaction(
+        function(tx) {
+           
+            var sql ="INSERT OR REPLACE INTO CalidadFuente (idfuente, tipo, numero, valor, fecha) VALUES (?, ?, ?, ?, ?)";
+            var val=document.getElementsByName("txttemp1")[0].value;	
+            var fu=localStorage.getItem("fuenteIDCalidad")
+            var t="temperatura";
+           // alert("ins"+fu+t+val);
+            var f = new Date();
+            var fec=f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear();
+            var params = [fu,t,1,val,fec];
+            tx.executeSql(sql, params);
 
+            var val2=document.getElementsByName("txttemp2")[0].value;
+            var params2 = [fu,t,2,val2,fec];
+            tx.executeSql(sql, params2);
+
+            var val3=document.getElementsByName("txttemp3")[0].value;
+            var params3 = [fu,t,3,val3,fec];
+            tx.executeSql(sql, params3);
+            
+        }
+    
+    );
+};
+
+function insertarCalidadFuenteTurbidez(){
+    var db = openDatabase('diteklocal', '1.0', 'DB', 2 * 1024 * 1024);
+    db.transaction(
+        function(tx) {
+           
+            var sql ="INSERT OR REPLACE INTO CalidadFuente (idfuente, tipo, numero, valor, fecha) VALUES (?, ?, ?, ?, ?)";
+            var val=document.getElementsByName("txtturb1")[0].value;	
+            var fu=localStorage.getItem("fuenteIDCalidad")
+            var t="turbidez";
+           // alert("ins"+fu+t+val);
+            var f = new Date();
+            var fec=f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear();
+            var params = [fu,t,1,val,fec];
+            tx.executeSql(sql, params);
+
+            var val2=document.getElementsByName("txtturb2")[0].value;
+            var params2 = [fu,t,2,val2,fec];
+            tx.executeSql(sql, params2);
+
+            var val3=document.getElementsByName("txtturb3")[0].value;
+            var params3 = [fu,t,3,val3,fec];
+            tx.executeSql(sql, params3);
+            
+        }
+    
+    );
+};
 function crearTablaCalidadFuente() {
     //borrarTablaFuentes();
    this.db.transaction(
@@ -84,10 +138,10 @@ function guardarMediciones() {
         insertarCalidadFuentePH();
     }
     if (localStorage.getItem("turbidez")=="0") {
-        
+        insertarCalidadFuenteTurbidez();
     }
     if (localStorage.getItem("temperatura")=="0") {
-        
+        insertarCalidadFuenteTemperatura();
     }
     if (localStorage.getItem("olorsabor")=="0") {
         
